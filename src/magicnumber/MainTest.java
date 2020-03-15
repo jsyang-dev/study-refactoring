75d869feda4cdf84f883e11cd3b1fd78fbae3f58
Jeongsu Yang <mycat83@gmail.com>
2020-03-15 11:14:41 +0900

    매직 넘버를 기호 상수로 치환
    리팩토링 전 코드 생성

---
+++ src/magicnumber/MainTest.java
@@ -0,0 +1,28 @@
+package magicnumber;
+
+import com.hyuki.refbook.StandardOutputTest;
+import junit.framework.JUnit4TestAdapter;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest extends StandardOutputTest {
+    @Test public void testMain() {
+        // Expected
+        String expected = getExpectedOutput(
+            "Andrew walks.",
+            "Andrew stops.",
+            "Andrew jumps.");
+
+        // Actual
+        Main.main(new String[0]);
+        String actual = getActualOutput();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(MainTest.class);
+    }
+}
