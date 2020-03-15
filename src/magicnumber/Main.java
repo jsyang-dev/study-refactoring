e53631337e19d0984a18d23d5744c5603da63cd6
Jeongsu Yang <mycat83@gmail.com>
2020-03-15 11:28:33 +0900

    매직 넘버를 기호 상수로 치환
    리팩토링 - 상수로 치환

--- src/magicnumber/Main.java
+++ src/magicnumber/Main.java
@@ -3,8 +3,8 @@ package magicnumber;
 public class Main {
     public static void main(String[] args) {
         Robot robot = new Robot("Andrew");
-        robot.order(0); // walk
-        robot.order(1); // stop
-        robot.order(2); // jump
+        robot.order(Robot.COMMAND_WALK);
+        robot.order(Robot.COMMAND_STOP);
+        robot.order(Robot.COMMAND_JUMP);
     }
 }
