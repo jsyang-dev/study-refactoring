8f107fc1c32d42096abf76d97ceb70c83e4257bb
Jeongsu Yang <mycat83@gmail.com>
2020-08-25 22:43:48 +0900

    제어 플래그 삭제
    * return을 사용 리팩토링

--- src/removecontrolflag/FindInt.java
+++ src/removecontrolflag/FindInt.java
@@ -2,14 +2,12 @@ package removecontrolflag;
 
 public class FindInt {
     public static boolean find(int[] data, int target) {
-        boolean found = false;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
-                found = true;
-                break;
+                return true;
             }
         }
-        return found;
+        return false;
     }
 }
 
