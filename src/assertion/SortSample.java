405fe4a621e94eea42346d7eb3b7bf88d522ce32
Jeongsu Yang <mycat83@gmail.com>
2020-08-27 22:44:52 +0900

    어서션 도입
    * 한단게 더 리팩토링

--- src/assertion/SortSample.java
+++ src/assertion/SortSample.java
@@ -1,32 +1,32 @@
 package assertion;
 
 public class SortSample {
-    private final int[] _data;
+    private final int[] data;
 
     public SortSample(int[] data) {
-        _data = new int[data.length];
-        System.arraycopy(data, 0, _data, 0, data.length);
+        this.data = new int[data.length];
+        System.arraycopy(data, 0, this.data, 0, data.length);
     }
 
     public void sort() {
-        for (int x = 0; x < _data.length - 1; x++) {
+        for (int x = 0; x < data.length - 1; x++) {
             int m = x;
-            for (int y = x + 1; y < _data.length; y++) {
-                if (_data[m] > _data[y]) {
+            for (int y = x + 1; y < data.length; y++) {
+                if (data[m] > data[y]) {
                     m = y;
                 }
             }
-            assert isMin(m, x, _data.length - 1);
-            int v = _data[m];
-            _data[m] = _data[x];
-            _data[x] = v;
-            assert isSorted(0, x + 1);
+            assert isMin(m, x, data.length - 1): this + ", x = " + x;
+            int v = data[m];
+            data[m] = data[x];
+            data[x] = v;
+            assert isSorted(0, x + 1): this + ", x = " + x;
         }
     }
 
     private boolean isMin(int pos, int start, int end) {
         for (int i = start; i <= end; i++) {
-            if (_data[pos] > _data[i]) {
+            if (data[pos] > data[i]) {
                 return false;
             }
         }
@@ -35,7 +35,7 @@ public class SortSample {
 
     private boolean isSorted(int start, int end) {
         for (int i = start; i < end; i++) {
-            if (_data[i] > _data[i + 1]) {
+            if (data[i] > data[i + 1]) {
                 return false;
             }
         }
@@ -45,8 +45,8 @@ public class SortSample {
     public String toString() {
         StringBuilder buffer = new StringBuilder();
         buffer.append("[ ");
-        for (int i = 0; i < _data.length; i++) {
-            buffer.append(_data[i]);
+        for (int datum : data) {
+            buffer.append(datum);
             buffer.append(", ");
         }
         buffer.append("]");
