e6fcda9a70693e80c2b86c8b0d0919de85cce9d9
Jeongsu Yang <mycat83@gmail.com>
2020-08-29 22:58:17 +0900

    메서드 추출
    * 리팩토링 - 메서드 추출

--- src/extractmethod/Banner.java
+++ src/extractmethod/Banner.java
@@ -1,28 +1,27 @@
 package extractmethod;
 
 public class Banner {
-    private final String _content;
+    private final String content;
     public Banner(String content) {
-        _content = content;
+        this.content = content;
     }
     public void print(int times) {
-        // 테두리 출력
+        printBorder();
+        printContent(times);
+        printBorder();
+    }
+
+    private void printBorder() {
         System.out.print("+");
-        for (int i = 0; i < _content.length(); i++) {
+        for (int i = 0; i < content.length(); i++) {
             System.out.print("-");
         }
         System.out.println("+");
+    }
 
-        // 내용 출력
+    private void printContent(int times) {
         for (int i = 0; i < times; i++) {
-            System.out.println("|" + _content + "|");
+            System.out.println("|" + content + "|");
         }
-
-        // 테두리 출력
-        System.out.print("+");
-        for (int i = 0; i < _content.length(); i++) {
-            System.out.print("-");
-        }
-        System.out.println("+");
     }
 }
