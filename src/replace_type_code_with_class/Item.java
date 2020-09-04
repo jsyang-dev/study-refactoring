58f078134111a70fb049d5408fe0a9be93101bf4
Jeongsu Yang <mycat83@gmail.com>
2020-09-04 23:03:47 +0900

    분류 코드를 클래스로 치환
    * 리팩토링 - 새로운 클래스 사용

--- src/replace_type_code_with_class/Item.java
+++ src/replace_type_code_with_class/Item.java
@@ -1,21 +1,21 @@
 package replace_type_code_with_class;
 
 public class Item {
-    public static final int TYPECODE_BOOK = 0;
-    public static final int TYPECODE_DVD = 1;
-    public static final int TYPECODE_SOFTWARE = 2;
+    public static final int TYPECODE_BOOK = ItemType.BOOK.getTypecode();
+    public static final int TYPECODE_DVD = ItemType.DVD.getTypecode();
+    public static final int TYPECODE_SOFTWARE = ItemType.SOFTWARE.getTypecode();
 
-    private final int _typecode;
+    private final ItemType itemType;
     private final String _title;
     private final int _price;
 
     public Item(int typecode, String title, int price) {
-        _typecode = typecode;
+        itemType = ItemType.getItemType(typecode);
         _title = title;
         _price = price;
     }
 
-    public int getTypecode() { return _typecode; }
+    public int getTypecode() { return itemType.getTypecode(); }
     public String getTitle() { return _title; }
     public int getPrice() { return _price; }
 
