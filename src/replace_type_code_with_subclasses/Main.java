9ef3fcaa9dec6f3061e128a8b6af34df1ab3afd6
Jeongsu Yang <mycat83@gmail.com>
2020-09-05 22:38:45 +0900

    분류 코드를 하위 클래스로 치환
    * 리팩토링 - switch 문 삭제

--- src/replace_type_code_with_subclasses/Main.java
+++ src/replace_type_code_with_subclasses/Main.java
@@ -2,12 +2,12 @@ package replace_type_code_with_subclasses;
 
 public class Main {
     public static void main(String[] args) {
-        Shape line = Shape.createShape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
-        Shape rectangle = Shape.createShape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
-        Shape oval = Shape.createShape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+        Shape line = Shape.createShapeLine(0, 0, 100, 200);
+        Shape rectangle = Shape.createShapeRectangle(10, 20, 30, 40);
+        Shape oval = Shape.createShapeOval(100, 200, 300, 400);
 
         Shape[] shape = {
-            line, rectangle, oval
+                line, rectangle, oval
         };
 
         for (Shape s : shape) {
--- src/replace_type_code_with_subclasses/Shape.java
+++ src/replace_type_code_with_subclasses/Shape.java
@@ -5,29 +5,29 @@ public abstract class Shape {
     public static final int TYPECODE_RECTANGLE = 1;
     public static final int TYPECODE_OVAL = 2;
 
-    private final int _startx;
-    private final int _starty;
-    private final int _endx;
-    private final int _endy;
+    private final int startx;
+    private final int starty;
+    private final int endx;
+    private final int endy;
 
-    public static Shape createShape(int typecode, int startx, int starty, int endx, int endy) {
-        switch (typecode) {
-            case TYPECODE_LINE:
-                return new ShapeLine(startx, starty, endx, endy);
-            case TYPECODE_RECTANGLE:
-                return new ShapeRectangle(startx, starty, endx, endy);
-            case TYPECODE_OVAL:
-                return new ShapeOval(startx, starty, endx, endy);
-            default:
-                throw new IllegalArgumentException("typecode = " + typecode);
-        }
+    public static ShapeLine createShapeLine(int startx, int starty, int endx, int endy) {
+        return new ShapeLine(startx, starty, endx, endy);
     }
 
+    public static ShapeRectangle createShapeRectangle(int startx, int starty, int endx, int endy) {
+        return new ShapeRectangle(startx, starty, endx, endy);
+    }
+
+    public static ShapeOval createShapeOval(int startx, int starty, int endx, int endy) {
+        return new ShapeOval(startx, starty, endx, endy);
+    }
+
+
     protected Shape(int startx, int starty, int endx, int endy) {
-        _startx = startx;
-        _endx = endx;
-        _starty = starty;
-        _endy = endy;
+        this.startx = startx;
+        this.endx = endx;
+        this.starty = starty;
+        this.endy = endy;
     }
 
     public abstract int getTypecode();
@@ -37,8 +37,8 @@ public abstract class Shape {
     public String toString() {
         return "[ "
                 + getName() + ", "
-                + "(" + _startx + ", " + _starty + ")-"
-                + "(" + _endx + ", " + _endy + ") ]";
+                + "(" + startx + ", " + starty + ")-"
+                + "(" + endx + ", " + endy + ") ]";
     }
 
     public abstract void draw();
